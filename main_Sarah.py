9e54835d2e84585d7f09220daa77d9fe5f4cd572
sarah-soliman1 <xvf6mm@virginia.edu>
2026-09-15 13:34:44 -0400

    Revisions/comments

--- main_Sarah.py
+++ main_Sarah.py
@@ -5,49 +5,60 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import statistics
-
+#imports necessary extensions and patient_sarah file
 
 Patient.instantiate_from_csv(
     "C:/Users/sarah/BME 2315/Module 1/BME2315_Module1/Metadata and Protein Data for Module 1.csv"
+) #opens csv data file
+
+#Sort & print useful patient data
+Patient.all_patients.sort(
+    key=Patient.get_age_of_death,
+    reverse=False
 )
+for patient in Patient.all_patients:
 
+    print(patient) 
 
+#Bar Graph
 
-female_dementia = []
+female_dementia = [] #empty list to iterate over for loop for female pt
 
-male_dementia = []
+male_dementia = [] #empty list to iterate over for loop for male pt
 
 
 for patient in Patient.filter(Patient.all_patients, sex="Female"):
-    female_dementia.append(int(patient.dementia))
+    female_dementia.append(int(patient.dementia))  #adds a 1 for female dementia pt and 0 for female non dementia pt
 
 
 for patient in Patient.filter(Patient.all_patients, sex="Male"):
-    male_dementia.append(int(patient.dementia))
+    male_dementia.append(int(patient.dementia))  #adds a 1 for male dementia pt and 0 for male non dementia pt
 
 
-x_Female_bar = statistics.mean(female_dementia) * 100
+x_Female_bar = statistics.mean(female_dementia) * 100  #calculates mean percentage of female with dementia
 
-x_Male_bar = statistics.mean(male_dementia) * 100
-
-
-female_stdev = statistics.stdev(female_dementia) * 100
-
-male_stdev = statistics.stdev(male_dementia) * 100
+x_Male_bar = statistics.mean(male_dementia) * 100 #calculates mean percentage of male with dementia
 
 
-print(f'x_Female_bar = {x_Female_bar}, female_stdev = {female_stdev}')
+
+female_stdev = statistics.stdev(female_dementia) * 100 #calculates stdevof perecentage of female with dementia
+
+
+male_stdev = statistics.stdev(male_dementia) * 100 #calculates stdevof perecentage of male with dementia
+
+
+print(f'x_Female_bar = {x_Female_bar}, female_stdev = {female_stdev}') #prints data that we are about to graph
 
 print(f'x_Male_bar = {x_Male_bar}, male_stdev = {male_stdev}')
 
 
-sex_cols = ["Female", "Male"]
+sex_cols = ["Female", "Male"] #names the columns
 
-mean_sex = [x_Female_bar, x_Male_bar]
+mean_sex = [x_Female_bar, x_Male_bar] #names y-axis values
 
-stdev_sex = [female_stdev, male_stdev]
+stdev_sex = [female_stdev, male_stdev] #names stdev values
 
-yerr = [np.zeros(len(mean_sex)), stdev_sex]
+yerr = [np.zeros(len(mean_sex)), stdev_sex] #sets up error bars
 
 
 plt.bar(
@@ -55,22 +66,22 @@ plt.bar(
     mean_sex,
     yerr=yerr,
     capsize=10
-)
+) #plots on a bar graph
 
-plt.title("Percentage of Patients with Dementia by Sex")
+plt.title("Percentage of Patients with Dementia by Sex") #title
 
-plt.xlabel("Sex")
+plt.xlabel("Sex") #label for x axis
 
-plt.ylabel("Percent with Dementia")
+plt.ylabel("Percent with Dementia") #label for y axis
 
-plt.show()
-
-
+plt.show() #shows us the graph
 
 
-age_death = []
+#Scatterplot
 
-abeta42_levels = []
+age_death = [] #intializes empty list for age of death data
+
+abeta42_levels = [] #intializes empty list for abeta level data
 
 
 for patient in Patient.all_patients:
@@ -79,30 +90,22 @@ for patient in Patient.all_patients:
 
         age_death.append(patient.age_of_death)
 
-        abeta42_levels.append(patient.abeta42)
+        abeta42_levels.append(patient.abeta42)#goes through for loop and adds data to each respective list if it's not an empty cell
 
 
-X = age_death
+X = age_death #defines x and y valyes for each variable
 
 y = abeta42_levels
 
 
-plt.scatter(X, y)
+plt.scatter(X, y) #plots values on scatterplot
 
-plt.xlabel("Age at Death")
+plt.xlabel("Age at Death") #adds x-axis label
 
-plt.ylabel("Amyloid-Beta42 (pg/ug)")
+plt.ylabel("Amyloid-Beta42 (pg/ug)") #adds y-axis label
 
-plt.title("Amyloid-Beta42 Levels vs Age at Death")
+plt.title("Amyloid-Beta42 Levels vs Age at Death") #adds title
 
-plt.show()
+plt.show() #shows us the graph
 
 
-Patient.all_patients.sort(
-    key=lambda patient: patient.age_of_death,
-    reverse=False
-)
-
-for patient in Patient.all_patients:
-
-    print(patient)
