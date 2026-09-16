b9d1b24eaab607c2f30f8b8cebdf19fd0ef107b1
Will-Barlow <utn6zu@virginia.edu>
2026-09-16 17:40:58 -0400

    Finished coding for Coding Homework-Patient Class Objects, Bar Graphs, and Scatterplots
    Finished coding according to rubric

--- Will_patient_code/main_will_patient.py
+++ Will_patient_code/main_will_patient.py
@@ -1,1 +1,88 @@
 from patient import * #importing data from patient.py
+
+import matplotlib.pyplot as plt #importing matplotlib for plotting bar graph
+import numpy as np 
+import statistics #importing statistics for calculating mean and standard deviation
+
+
+patient_objects.instantiate_from_csv("/Users/williambarlow/Library/CloudStorage/OneDrive-UniversityofVirginia/Second Year/First Semester/BME 2315/Module 1/BME2315_Module1/Metadata and Protein Data for Module 1.csv") #instantiating patient objects from the csv file
+
+
+#Question 4: Printing patient objects from csv
+for patient in patient_objects.all_patients:
+    print(patient) #printing list of patient objects for each patient
+
+
+
+#Question 5: Sort patients by age of diagnosis from youngest to oldest, moves empty values to the end of the list
+patient_objects.all_patients.sort(
+    key=patient_objects.get_age_of_diagnosis,
+    reverse=False
+)
+
+for patient in patient_objects.all_patients:
+    print(patient) #printing list of patient objects for each patient sorted by age of diagnosis from youngest to oldest
+
+
+#Question 6: Filter patients by age of diagnosis and age of death
+
+filtered_patients = patient_objects.filter_age_range(80, 90, 85, 95) #filtering patients by age of diagnosis between 80 and 90 and age of death between 85 and 95
+
+for patient in filtered_patients:
+    print(patient) #printing list of patient objects for each patient filtered by age of diagnosis and age of death
+
+
+#Question 7: Create bar graph with standard deviation for age of symptom onset for male and female patients
+
+age_female_patients = [] #creating an empty list to store age of symptom onset for female patients
+age_male_patients = [] #creating an empty list to store age of symptom onset for male
+
+for patient in patient_objects.all_patients:
+    if patient.sex == "Female" and patient.age_of_symptom_onset != "":
+        age_female_patients.append(patient.age_of_symptom_onset) #appending list of age of symptom onset for female patients
+
+for patient in patient_objects.all_patients:
+    if patient.sex == "Male" and patient.age_of_symptom_onset != "":
+        age_male_patients.append(patient.age_of_symptom_onset) #appending list of age of symptom onset for male patients
+
+#calculates mean symptom onset age for male and female patients
+female_mean = statistics.mean(age_female_patients)  
+male_mean = statistics.mean(age_male_patients)
+
+#calculates standard deviation for symptom onset age for male and female patients
+female_stdev = statistics.stdev(age_female_patients)
+male_stdev = statistics.stdev(age_male_patients)
+
+patient_sex = ["Female", "Male"]
+
+mean_onset_age = [female_mean, male_mean]
+stdev_onset_age = [female_stdev, male_stdev]
+
+#creates bar graph with bar height as mean symptom onset age and error bars as plus and minus one standard deviation
+plt.bar(patient_sex, mean_onset_age, yerr=stdev_onset_age, capsize=10)
+
+plt.title("Average Age of Symptom Onset by Sex")
+plt.xlabel("Sex")
+plt.ylabel("Average Age of Symptom Onset (Years)")
+
+plt.show()
+
+
+#Question 8: Scatterplot comparing age of diagnosis and age of death
+
+diagnosis_ages = []
+death_ages = []
+
+#only displays patients with both diagnosis and death ages so all points represent one patient
+for patient in patient_objects.all_patients:
+    if patient.age_of_diagnosis != "" and patient.age_of_death != "":
+        diagnosis_ages.append(patient.age_of_diagnosis) #appending list of age of diagnosis for patients
+        death_ages.append(patient.age_of_death) #appending list of age of death for patients
+
+#creates scatterplot for relationship between age of diagnosis and age of death for patients
+plt.scatter(diagnosis_ages, death_ages)
+plt.title("Age of Dementia Diagnosis vs Age of Death")
+plt.xlabel("Age of Dementia Diagnosis (Years)")
+plt.ylabel("Age of Death (Years)")
+
+plt.show()
--- Will_patient_code/patient.py
+++ Will_patient_code/patient.py
@@ -3,30 +3,67 @@ import csv #importing data from Metadata and Protein Data for Module 1 csv
 
 class patient_objects: #created class for pateint objects
 
-    def __init__(self, age_of_symptom_onset: int, age_of_diagnosis: int, age_of_death: int): #created the constructor for patient class which will take inputs for age of symptom onset, age of diagnosis, and age of death as integers
+    all_patients = [] #creating an empty list to store patient objects
+
+    def __init__(self, age_of_symptom_onset: int, age_of_diagnosis: int, age_of_death: int, sex:str): #created the constructor for patient class which will take inputs for age of symptom onset, age of diagnosis, and age of death as integers, later added sex for question 7
         self.age_of_symptom_onset = age_of_symptom_onset
         self.age_of_diagnosis = age_of_diagnosis
         self.age_of_death = age_of_death
+        self.sex = sex
+
+        patient_objects.all_patients.append(self) #appending the patient object to the all_patients list
 
     def __repr__(self):  
-            return f"Patient: ({self.age_of_symptom_onset} | {self.age_of_diagnosis} | {self.age_of_death})"
-
-patients = [] #creating an empty list to store patient objects
-
-with open("/Users/williambarlow/Library/CloudStorage/OneDrive-UniversityofVirginia/Second Year/First Semester/BME 2315/Module 1/BME2315_Module1/Metadata and Protein Data for Module 1.csv", newline="") as f: #opening the csv file
-    reader = csv.DictReader(f) #reading the csv file
+            return f"Patient: ({self.age_of_symptom_onset} | {self.age_of_diagnosis} | {self.age_of_death} | {self.sex})" #returning the string representation of the patient object
 
 
+    def get_age_of_diagnosis(self):# Returns diagnosis age to sort, moves empty values to end of the list
+            if self.age_of_diagnosis == "":
+                return 1000# python returns a large number for empty values so that they are moved to the end of the list when sorted
+            else:
+                return self.age_of_diagnosis
 
-    for row in reader: #iterating through the rows of the csv file
-         patient = patient_objects(
-              int(row["Age of onset cognitive symptoms"]),
-              int(row["Age of Dementia diagnosis"]),
-              int(row["Age at death"])
+    
+             
+    @classmethod
+    def instantiate_from_csv(cls, filename):
 
-            ) #creating a patient object for each row in the csv file and appending it to the patients list
+         with open(filename, "r") as f:
+              reader = csv.DictReader(f)
 
-    patients.append(patient)
+              for row in reader: #iterating through the rows of the csv file
+                       patient = patient_objects(
+                            convert_age(row["Age of onset cognitive symptoms"]),
+                            convert_age(row["Age of Dementia diagnosis"]),
+                            convert_age(row["Age at Death"]),
+                            row["Sex"]
+              
+                          ) #creating a patient object for each row in the csv file and appending it to the patients list
 
-    for patient in patients:
-         print(patient)
+
+    @classmethod
+    def filter_age_range(cls, diagnosis_min, diagnosis_max, death_min, death_max):
+        filtered_patients = []
+
+        for patient in cls.all_patients:
+            if patient.age_of_diagnosis != "" and patient.age_of_death != "":
+                if (diagnosis_min <= patient.age_of_diagnosis <= diagnosis_max
+                        and death_min <= patient.age_of_death <= death_max):
+                    filtered_patients.append(patient)
+
+        return filtered_patients
+
+
+              
+
+def convert_age(value): #keeps missing csv ages as blank strings and converts recorded ages to integers
+    if value == "":
+        return ""
+    else:
+        return int(value)
+
+
+
+
+         
+
